02889bed0042baf76714dc07a6f1c44598c3b483
Isaiah Rappaport <irappap98@gmail.com>
2021-12-02 22:05:59 -0500

    pull vue data from GIT API

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,42 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <br/>
+    <h2 class="vue">Vue.Js</h2>
+    <h2> {{vue_data.forks_count}} </h2>
+    <h2> {{vue_data.watchers}} </h2>
+    <h2> {{vue_data.stargazers_count}} </h2>
   </div>
 </template>
 
 <style>
+.vue {
+  text-decoration: underline;
+}
 </style>
 
 <script>
+import axios from "axios";
+
 export default {
   data: function () {
     return {
       message: "JS Framework Watcher",
+      vue_data: "",
     };
   },
-  created: function () {},
-  methods: {},
+  created: function () {
+    this.vueID();
+  },
+
+  methods: {
+    // vueID
+    vueID: function () {
+      axios.get("https://api.github.com/repos/vuejs/vue").then((response) => {
+        console.log("id", response.data);
+        this.vue_data = response.data;
+      });
+    },
+  },
 };
 </script>
